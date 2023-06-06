822965440395872a015a716a01e28a11b86aedb0
jasmin-raith <jasmin-raith@outlook.com>
2023-06-06 13:57:01 +0200

    empty files added

---
+++ script.js
@@ -0,0 +1,13 @@
+async function includeHTML() {
+    let includeElements = document.querySelectorAll('[w3-include-html]');
+    for (let I = 0; I < includeElements.length; I++) {
+        const element = includeElements[I];
+        file = element.getAttribute('w3-include-html');   // „includes/header.html“*
+        let resp = await fetch(file);
+        if (resp.ok) {
+            element.innerHTML = await resp.text();
+        } else {
+            element.innerHTML = 'Page not found';
+        }
+    }
+}
